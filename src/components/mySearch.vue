1869db6d90d11813273810d2c1851ee0b59a4eee
zdy <1084575562@qq.com>
2017-12-24 22:27:19 +0800

    index search

--- src/components/mySearch.vue
+++ src/components/mySearch.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="input-box">
-    <div class="input-default">
-      <i class="glyphicon glyphicon-search"></i>
-      <span>搜索</span>
+    <div class="input-default-box">
+      <div class="input-default">
+        <i class="glyphicon glyphicon-search"></i>
+        <span>搜索</span>
+      </div>
     </div>
   </div>
 </template>
